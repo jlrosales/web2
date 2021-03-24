0ccfe9904f967cda18c151bb9d5db42d4b5aed8d
jlrosales <62244921+jlrosales@users.noreply.github.com>
2021-03-24 15:45:05 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3551,7 +3551,7 @@ function generaError() {
 }
 
 function generaDatos(elemento) {
-    alert(elemento.Nombre+ " "+elemento.Apellidos+" su curso es "+elemento.Curso+" y la jornada es "+elemento.Jornada)
+    alert(elemento.Nombre.toUpperCase()+ " "+elemento.Apellidos..toUpperCase()+" su curso es "+elemento.Curso.toUpperCase()+" y la jornada es "+elemento.Jornada.toUpperCase())
 
 }
 
